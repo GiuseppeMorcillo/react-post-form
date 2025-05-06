9f5a564a75979c7380fd3faa741f711f30352b81
GiuseppeMorcillo <beppebeppe18@gmail.com>
2025-05-06 10:25:51 +0200

    feat:  aggiunto le rotte per i vari post

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route,   Link } from 'react-router-dom';
 import PostForm from '../components/PostForm';
 import PostList from '../components/PostList';
 import PostDetail from '../components/PostDetail';
